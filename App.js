fe5ef249f34933983d54fa92a48fdb569e6aa8ea
ErikFer22 <106500624+ErikFer22@users.noreply.github.com>
2023-03-16 08:14:38 -0600

    Login bien

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login from './components/Screens/login';
+import singup from './components/Screens/singup';
+import post from './components/Screens/Post';
+import HomeScreen from './components/Screens/HomeScreen';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <NativeBaseProvider>
+    <Stack.Navigator initialRouteName='login'>
+      <Stack.Screen
+        name="login"
+        component={login}
+        options={{title: 'login'}}
+      />
+      <Stack.Screen 
+        name="singup"
+        component={singup}
+        options={{ title: 'sing-up' }}
+      />
+      <Stack.Screen 
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Home' }}
+      />
+      <Stack.Screen 
+        name="Post"
+        component={post}
+        options={{ title: 'Post' }}
+      />
+      
+    </Stack.Navigator>
+    </NativeBaseProvider>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
