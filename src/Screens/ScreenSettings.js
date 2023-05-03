2b0d9fc17fb073335d333ee1bd90444e5ad3dad3
Jenny-Caro <jennifercarolinardz2@gmail.com>
2023-05-03 15:46:34 -0600

    Jennifer-commit settings

--- src/Screens/ScreenSettings.js
+++ src/Screens/ScreenSettings.js
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+
+import { Box, Text, HStack, Icon, Menu, Pressable, Center,Image,VStack,Button } from "native-base";
+// MaterialIcons 
+import {  MaterialIcons } from '@expo/vector-icons';
 
 // Native base imports
-import { Button } from "native-base";
-
 const ScreenSettings = ({ navigation }) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    
+    <View style={{flex: 1, justifyContent: 'justify', alignItems: 'justify'}}>
+      <StatusBar  />
+        <HStack bg="gray.300" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="100%">
+        <HStack alignItems="center">
+          <Text color="black" fontSize="20" fontWeight="bold">
+            Scar
+          </Text>
+        </HStack>
+        <HStack>
+          <Menu 
+            w="190" 
+            trigger={triggerProps => {
+              return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+                      <Icon as={MaterialIcons} name="more-vert" size="sm" color="white" />
+                    </Pressable>
+              }}>
+            <Menu.Item onPress={() => navigation.navigate(screen.settings)}>Settings</Menu.Item>
+          </Menu>
+        </HStack>
+       </HStack>
+       <Text>Idioma</Text>
+       <Text>Modo obscuro</Text>
       <Text>Settings Screen</Text>
       <Button mt='5' onPress={() => navigation.goBack()}>Go Back</Button>
     </View>
