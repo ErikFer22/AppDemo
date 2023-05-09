053979659b75a1fd0537e7583c1cc5ed9e634229
Jenny-Caro <jennifercarolinardz2@gmail.com>
2023-05-08 22:54:05 -0600

    commit-jenny

--- src/Screens/Board1.js
+++ src/Screens/Board1.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar';
 
 
 // Native base imports
@@ -8,8 +9,14 @@ import { Button  } from "native-base";
 const Board = ({ navigation }) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <StatusBar  />
       <Text>Screen Board</Text>
-    
+      <Image
+          style={{ width: 200, height: 200, marginBottom: 10 }}
+          source={{ uri: "https://cdn.pixabay.com/photo/2013/07/12/18/30/rpm-153397_1280.png" }}
+        />
+      
+      <Text>0 0 km/h</Text>
       <Button mt='5' onPress={() => navigation.goBack()}>Go menu</Button>
     </View>
   )
