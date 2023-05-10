a01b834df303c691eb3780dbdd75a7e5e9d2d8c0
BennettxD2001 <rbennettito1@gmail.com>
2023-05-09 22:41:14 -0600

    Cambio perfil

--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,14 +1,40 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 
 // Native base imports
-import { Button } from "native-base";
+import { Box, HStack, Text,  Icon, Menu, Pressable, Center,Image,VStack,Button } from "native-base";
 
 const Profile = ({ navigation }) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Favorite Screen</Text>
-      <Button mt='5' onPress={() => navigation.goBack()}>Go Back</Button>
+      <HStack w="64" h="20" justifyContent="center">
+         <Image 
+         size={100} 
+         borderRadius={100} source={{
+          uri: "https://img.freepik.com/vector-premium/icono-perfil-avatar_188544-4755.jpg?w=2000"
+        }} alt="Foto de perfil" />
+      </HStack>
+      <VStack space={2} alignItems="center">
+        <Text bold>
+          email@gmail.com
+        </Text>
+        
+        <Text italic>
+          Nombre y apellido
+        </Text>
+
+        <Text italic>
+          Tu auto es un : 
+        </Text>
+      </VStack>
+      <HStack>
+        <VStack>
+          <Button  mt='10' onPress={() => navigation.navigate(screen.cardata)}>Cambiar de auto</Button>
+        </VStack>
+        <VStack>
+          <Button  mt='10' onPress={() => navigation.navigate(screen.login)}>Cerrar sesión</Button>
+        </VStack>
+      </HStack>
     </View>
   )
 }
